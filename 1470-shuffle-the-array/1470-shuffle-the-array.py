b00fa67464c1a0c03377791b8feaa221b379bb40
Aditya Aryan <25f2001442@ds.study.iitm.ac.in>
2026-07-23 17:54:45 +0530

    Time: 37 ms (78.42%), Space: 12.5 MB (81.41%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def shuffle(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: List[int]
-        """
         output = []
         for i in range(n):
             output.append(nums[i])
